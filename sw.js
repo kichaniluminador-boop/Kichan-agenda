1e947c212b8683d019d597c06f0b7ebd7ff85c53
kichaniluminador-boop <kichan.iluminador@gmail.com>
2026-09-18 07:43:36 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='kichan-agenda-v1';const A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
